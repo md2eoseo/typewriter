79b5a1d774c7d52df0b030c15c5ca60f0aff3e6d
md2eoseo <md2eoseo@gmail.com>
2020-02-10 20:13:32 +0100

    better typing sounds

--- typewriter.js
+++ typewriter.js
@@ -1,22 +1,42 @@
 "use strict";
 window.addEventListener("DOMContentLoaded", start);
 const typewritten = document.querySelector(".typewritten");
-const str = typewritten.textContent;
+const str = typewritten.textContent.trim();
+const len = str.length;
 let i = 0;
 typewritten.innerHTML = "";
+console.log(str + "'s length is " + len);
 
 function start() {
-  if (i < str.length) {
+  if (i < len) {
+    sound();
     typewritten.innerHTML += str[i++];
     // document.querySelector("#typekey1").pause();
-    sound();
+  } else {
+    document.querySelector("#typelast").play();
+    console.log("last");
   }
 }
 
 function sound() {
-  if (str[i - 1] == " ") document.querySelector("#typespace").play();
-  else if ((i - 1) % 2 == 1) document.querySelector("#typekey1").play();
-  else document.querySelector("#typekey2").play();
+  if ((i - 1) % 2 == 0) {
+    document.querySelector("#typekey1").pause();
+    console.log("1 pause");
+  } else {
+    document.querySelector("#typekey2").pause();
+    console.log("2 pause");
+  }
+
+  if (str[i] == " ") {
+    document.querySelector("#typespace").play();
+    console.log("space");
+  } else if (i % 2 == 1) {
+    document.querySelector("#typekey1").play();
+    console.log("1");
+  } else {
+    document.querySelector("#typekey2").play();
+    console.log("2");
+  }
 
   setTimeout(start, 200);
 }
