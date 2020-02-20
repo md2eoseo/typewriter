47e2a2c1b388acef386531811e564e697383a879
md2eoseo <md2eoseo@gmail.com>
2020-02-20 10:35:19 +0100

    v2 without global variables, just 3 funcs, used closure

--- typewriter.js
+++ typewriter.js
@@ -1,42 +1,48 @@
 "use strict";
 window.addEventListener("DOMContentLoaded", start);
-const typewritten = document.querySelector(".typewritten");
-const str = typewritten.textContent.trim();
-const len = str.length;
-let i = 0;
-typewritten.innerHTML = "";
-console.log(str + "'s length is " + len);
 
 function start() {
-  if (i < len) {
-    sound();
-    typewritten.innerHTML += str[i++];
-    // document.querySelector("#typekey1").pause();
-  } else {
-    document.querySelector("#typelast").play();
-    console.log("last");
-  }
+  const typewritten = document.querySelector(".typewritten");
+  typewriter(typewritten, done);
 }
 
-function sound() {
-  if ((i - 1) % 2 == 0) {
-    document.querySelector("#typekey1").pause();
-    console.log("1 pause");
-  } else {
-    document.querySelector("#typekey2").pause();
-    console.log("2 pause");
-  }
+function done() {
+  console.log("Done!");
+}
 
-  if (str[i] == " ") {
-    document.querySelector("#typespace").play();
-    console.log("space");
-  } else if (i % 2 == 1) {
-    document.querySelector("#typekey1").play();
-    console.log("1");
-  } else {
-    document.querySelector("#typekey2").play();
-    console.log("2");
-  }
+function typewriter(typewritten, callback) {
+  const str = typewritten.textContent.trim();
+  let i = 0;
+  typewritten.innerHTML = "";
 
-  setTimeout(start, 200);
+  loop();
+
+  function loop() {
+    if ((i - 1) % 2 == 0) {
+      document.querySelector("#typekey1").pause();
+      console.log("1 pause");
+    } else {
+      document.querySelector("#typekey2").pause();
+      console.log("2 pause");
+    }
+
+    if (str[i] == " ") {
+      document.querySelector("#typespace").play();
+      console.log("space");
+    } else if (i % 2 == 1) {
+      document.querySelector("#typekey1").play();
+      console.log("1");
+    } else {
+      document.querySelector("#typekey2").play();
+      console.log("2");
+    }
+
+    typewritten.innerHTML += str[i++];
+
+    if (i < str.length) setTimeout(loop, 200);
+    else {
+      document.querySelector("#typelast").play();
+      callback();
+    }
+  }
 }
